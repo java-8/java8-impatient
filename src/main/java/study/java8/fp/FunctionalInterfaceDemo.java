ecb1420a7391c12dd1986a4914d3c23f4d7c3605
之剑 <guangjian.cgj@alibaba-inc.com>
2016-08-18 13:18:53 +0800

    命名

--- src/main/java/study/java8/fp/FunctionalInterfaceDemo.java
+++ src/main/java/study/java8/fp/FunctionalInterfaceDemo.java
@@ -6,23 +6,23 @@ package study.java8.fp;
 /**
  * @author jack 2016年8月16日 下午3:57:58
  */
-public class DoubleColons {
+public class FunctionalInterfaceDemo {
 	String startWith(String s) {
 		return s.valueOf(s.charAt(0));
 	}
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DoubleColons DoubleColons = new DoubleColons();
+		FunctionalInterfaceDemo DoubleColons = new FunctionalInterfaceDemo();
 		Converter<String, String> converter = DoubleColons::startWith;
-
+		
 		String cc = converter.convert("Scala");
 		System.out.println(cc);
-
+		
 	}
-
+	
 }
 
 @FunctionalInterface
